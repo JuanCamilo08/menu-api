cf22880dfd1c4645e5873d21195983e6b51be8bd
JuanCamilo08 <jcamilo@intellisys.com.do>
2020-02-27 13:16:08 -0400

    create the ingredient model

---
+++ models/ingredient.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  description: {
+    type: String,
+    minlength: 5,
+    maxlength: 200
+  }
+}) 
+
+module.exports.Ingredient = mongoose.model('ingredient', schema);
+
+module.exports.validateIngredient = function(ingredient){
+  const schema = Joi.Object({
+    name: Joi.string().min(5).max(50).required(),
+    description: Joi.string().min(5).max(200)
+  })
+
+  return schema.validate(ingredient);
+}
+
